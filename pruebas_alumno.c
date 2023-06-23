3939786ca4b209a5ed6b6a09d427859e9be2a760
sofiajaves <83650724+sofiajaves@users.noreply.github.com>
2023-06-23 12:37:47 -0300

    Add files via upload

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,55 @@
+#include "src/hash.h"
 #include "pa2m.h"
 
-void prueba_simple()
+void probar_creacion()
 {
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i != j, "i=14 es diferente de j=16");
+	pa2m_nuevo_grupo("PRUEBAS DE CREACION");
+
+	hash_t *hash = hash_crear(1);
+	pa2m_afirmar(hash != NULL,
+		     "Se puede crear un hash solicitando capacidad menor a 3");
+	hash_destruir(hash);
+
+	hash = hash_crear(8);
+	pa2m_afirmar(hash != NULL,
+		     "se puede crear un hash solicitando capacidad mayor a 3");
+
+	pa2m_afirmar(hash_cantidad(hash) == 0,
+		     "un hash recien creado tiene 0 elementos");
+
+	hash_destruir(hash);
+}
+
+void probar_insertar()
+{
+	pa2m_nuevo_grupo("PRUEBAS DE INSERTAR");
+	hash_t *hash = hash_crear(1);
+	pa2m_afirmar(hash_insertar(hash, "1", "UNO", NULL) != NULL,
+		     "Se inserta un elemento ");
+	pa2m_afirmar(hash_insertar(hash, "2", "DOS", NULL) != NULL,
+		     "Se inserta un elemento");
+	pa2m_afirmar(hash_insertar(hash, "3", "TRES", NULL) != NULL,
+		     "Se inserta un elemento");
+	pa2m_afirmar(hash_cantidad(hash) == 3, "El hash contiene 3 elementos ");
+
+	pa2m_afirmar(hash_insertar(hash, "4", "CUATRO", NULL) != NULL,
+		     "Se inserta un elemento ");
+	pa2m_afirmar(hash_cantidad(hash) == 4, "El hash contiene 4 elementos ");
+
+	pa2m_afirmar(hash_insertar(hash, "4", "CUATRO1", NULL) != NULL,
+		     "Se inserta un elemento ");
+	pa2m_afirmar(hash_cantidad(hash) == 4, "El hash contiene 4 elementos ");
+
+	pa2m_afirmar(hash_contiene(hash, "4") == true,
+		     "El hash contiene el elemento con clave 4");
+
+	hash_destruir(hash);
 }
 
 int main()
 {
-	pa2m_nuevo_grupo(
-		"\n======================== XXX ========================");
-	prueba_simple();
+	probar_creacion();
+	probar_insertar();
 
 	return pa2m_mostrar_reporte();
 }
